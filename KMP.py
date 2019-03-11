39f8fe81799a3bfb17c3dbdcd170b0295b8b76bc
Christian <c_kennedy@coloradaocollege.edu>
2019-03-11 10:03:43 -0600

    updated KMP algorithm code

--- KMP.py
+++ KMP.py
@@ -1,56 +1,49 @@
 import time
 
-# KMP Algorithm string-matching method
+
+# KMP Algorithm string-matching method - prints out the index in the input text where the search pattern was found
 # @param pat - the string pattern to search for
-# @param txt - the text to search for the pattern in
+# @param txt - the input text to search for the pattern in
 def KMPSearch(pat, txt):
+    M = len(pat)    # length of the search pattern
+    N = len(txt)    # length of the input string
+    lps = [0]*M     # create lps[] that will hold the longest prefix suffix values for pattern matching
+    j = 0           # current index for pat[]
 
-    M = len(pat)
-    N = len(txt)
+    computeLPSArray(pat, M, lps)    # Preprocess the pattern and calculate lps[] array
 
-# create lps[] that will hold the longest prefix suffix values for pattern matching
-    lps = [0]*M
-    j = 0 # index for pat[]
-
-# Preprocess the pattern and calculate lps[] array
-    computeLPSArray(pat, M, lps)
-
-    i = 0 # index for txt[]
+    i = 0           # current index for txt[]
     while i < N:
         if pat[j] == txt[i]:
             i += 1
             j += 1
-
         if j == M:
             z = str(i-j)
             print("Found pattern at index " + z)
             j = lps[j-1]
-
-        # mismatch after j matches
-        elif i < N and pat[j] != txt[i]:
-            # Do not match lps[0..lps[j-1]] characters, as they will match anyway
+        elif i < N and pat[j] != txt[i]:    # mismatch after j matches
+                                            # Do not match lps[0..lps[j-1]] characters, as they will match anyway
             if j != 0:
                 j = lps[j-1]
             else:
                 i += 1
 
+# KMP algorithm pattern preprocessing method - builds the lps[] for the search pattern
+# @param pat - the search pattern string
+# @param M - the length of the search pattern string
+# @param lps - the array for holding the lps[] for the search pattern string
 def computeLPSArray(pat, M, lps):
     len = 0 # length of the previous longest prefix suffix
     lps[0] # lps[0] is always 0
     i = 1
-
-    # the loop calculates lps[i] for i = 1 to M-1
-    while i < M:
+    while i < M:        # the loop calculates lps[i] for i = 1 to M-1
         if pat[i]== pat[len]:
             len += 1
             lps[i] = len
             i += 1
         else:
-            # This is tricky. Consider the example: AAACAAAA and i = 7. The idea is similar to search step.
             if len != 0:
                 len = lps[len-1]
-
-                # Also, note that we do not increment i here
             else:
                 lps[i] = 0
                 i += 1
